fa74bb5613fc02822bd3b1898f25729eab928646
pvasudevan2 <69927853+pvasudevan2@users.noreply.github.com>
2023-12-08 08:01:52 -0600

    Add basic assertion to test 1

--- tests/test_1.js
+++ tests/test_1.js
@@ -1,8 +1,9 @@
 const { Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
 
 // const (Builder) = require ("selenium-webdriver");
 
-async function test_1() {
+async function test_1(email, pass) {
 
     // launch the browser
     let driver = await new Builder().forBrowser("chrome").build();
@@ -28,6 +29,16 @@ async function test_1() {
     // type pass into "Password" input box
     const passInput = await driver.findElement(By.id("password"));
     await passInput.sendKeys(pass, Key.RETURN);
+
+    // wait for 10 seconds for things to load
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // assert
+    const expectedURL = "https://www.hudl.com/home";
+    const currentURL = await driver.getCurrentUrl();
+    
+    assert.strictEqual(currentURL, expectedURL);
+
 
     // close the browswer
     await driver.quit();
